ad31081b2211a5d910b9c03c4f81a33d830eb968
Hidenori Miyoshi <miyohide@gmail.com>
2020-04-20 21:31:08 +0900

    define totalPhotos

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,5 +1,10 @@
 module Types
   class QueryType < Types::BaseObject
+    field :totalPhotos, Int, null: false
+    def total_photos
+      42
+    end
+
     field :allPhotos, [Types::PhotoType], null: false
     def all_photos
       Photo.all
