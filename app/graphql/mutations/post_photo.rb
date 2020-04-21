ba8299a051ee234ae745753d42a4cfc6968d0152
Hidenori Miyoshi <miyohide@gmail.com>
2020-04-21 21:11:55 +0900

    delete url from post_photo mutation

--- app/graphql/mutations/post_photo.rb
+++ app/graphql/mutations/post_photo.rb
@@ -5,12 +5,11 @@ module Mutations
     field :photo, Types::PhotoType, null: true
     field :result, Boolean, null: true
 
-    argument :url, String, required: true
     argument :name, String, required: true
     argument :description, String, required: false
 
     def resolve(**args)
-      photo = Photo.create(url: args[:url], name: args[:name], description: args[:description])
+      photo = Photo.create(name: args[:name], description: args[:description])
       {
           photo: photo,
           result: photo.errors.blank?
