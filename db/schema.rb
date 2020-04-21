a931f53f4293230c39c8a60f54db9405da6f92ad
Hidenori Miyoshi <miyohide@gmail.com>
2020-04-21 20:56:38 +0900

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_20_120827) do
+ActiveRecord::Schema.define(version: 2020_04_21_115538) do
 
   create_table "photos", force: :cascade do |t|
-    t.string "url"
     t.string "name"
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
