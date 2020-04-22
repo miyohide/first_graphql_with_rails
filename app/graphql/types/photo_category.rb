fa06ae1658a79f8215c97385e334d9665d3c6960
Hidenori Miyoshi <miyohide@gmail.com>
2020-04-22 20:04:40 +0900

    define PhotoCategory enum.

---
+++ app/graphql/types/photo_category.rb
@@ -0,0 +1,7 @@
+class Types::PhotoCategory < Types::BaseEnum
+  value "SELFIE", "SELFIE"
+  value "PORTRAIT", "PORTRAIT"
+  value "ACTION", "ACTION"
+  value "LANDSCAPE", "LANDSCAPE"
+  value "GRAPHIC", "GRAPHIC"
+end
