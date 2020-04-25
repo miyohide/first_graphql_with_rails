9b3be5d4b60fae2d23e772d8777400fb742d8748
Hidenori Miyoshi <miyohide@gmail.com>
2020-04-25 11:48:43 +0900

    create db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+mHattrup = User.create(github_login: 'mHattrup', name: 'Mike Hattrup')
+gPlake = User.create(github_login: 'gPlake', name: 'Glen Plake')
+sSchmidt = User.create(github_login: 'sSchmidt', name: 'Scot Schmidt')
+
+Photo.create(
+    name: 'Dropping the Heart Chute',
+    description: 'The heart chute is one of my favorite chutes',
+    category: 'ACTION',
+    user: gPlake)
+
+Photo.create(
+    name: 'Enjoying the sunshine',
+    category: 'SELFIE',
+    user: sSchmidt)
+
+Photo.create(
+    name: 'Gunbarrel 25',
+    description: '25 laps on gunbarrel today',
+    category: 'LANDSCAPE',
+    user: sSchmidt)
