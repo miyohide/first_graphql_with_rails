b4bbd619d81e215b624086565912e55f486138e2
Hidenori Miyoshi <miyohide@gmail.com>
2020-04-25 18:03:59 +0900

    define inPhotos

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -4,9 +4,14 @@ module Types
     field :name, String, null: false
     field :avatar, String, null: true
     field :postedPhotos, [Types::PhotoType], null: true
+    field :inPhotos, [Types::PhotoType], null: false
 
     def posted_photos
       object.photos
+    end
+
+    def in_photos
+      object.in_photos
     end
   end
 end
