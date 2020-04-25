352f140003baba92359686fd316cdc06547983d5
Hidenori Miyoshi <miyohide@gmail.com>
2020-04-25 11:01:56 +0900

    change options

--- db/migrate/20200425015348_add_user_ref_to_photos.rb
+++ db/migrate/20200425015348_add_user_ref_to_photos.rb
@@ -1,5 +1,5 @@
 class AddUserRefToPhotos < ActiveRecord::Migration[6.0]
   def change
-    add_reference :photos, :user, null: false, foreign_key: true
+    add_reference :photos, :user, foreign_key: true
   end
 end
