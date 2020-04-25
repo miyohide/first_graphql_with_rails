519079bc1622fc0b73310caea33a49a6cbeb4915
Hidenori Miyoshi <miyohide@gmail.com>
2020-04-25 16:47:17 +0900

    fix typo

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -2,7 +2,7 @@ module Types
   class UserType < Types::BaseObject
     field :github_login, ID, null: false
     field :name, String, null: false
-    field :avator, String, null: true
+    field :avatar, String, null: true
     field :postedPhotos, [Types::PhotoType], null: true
   end
 end
