7d0f6ea3d9580d862cbdd88c240c28c13c632d03
Hidenori Miyoshi <miyohide@gmail.com>
2020-04-25 16:48:39 +0900

    add postedBy field

--- app/graphql/types/photo_type.rb
+++ app/graphql/types/photo_type.rb
@@ -5,9 +5,14 @@ module Types
     field :name, String, null: false
     field :description, String, null: true
     field :category, Types::PhotoCategory, null: false
+    field :postedBy, Types::UserType, null: false
 
     def url
       "http://yoursite.com/img/#{object.id}.jpg"
+    end
+
+    def posted_by
+      object.user
     end
   end
 end
