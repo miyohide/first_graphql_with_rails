17aab37efa16cb4553fba35831f64e9bf2e1c968
Hidenori Miyoshi <miyohide@gmail.com>
2020-04-30 11:38:16 +0900

    add create_user spec.

--- app/graphql/mutations/create_user.rb
+++ app/graphql/mutations/create_user.rb
@@ -7,14 +7,19 @@ module Mutations
     argument :name, String, required: true
     argument :auth_provider, AuthProviderSignupData, required: false
 
-    type Types::UserType
+    field :user, Types::UserType, null: true
+    field :result, Boolean, null: true
 
     def resolve(name: nil, auth_provider: nil)
-      User.create!(
+      u = User.create(
           name: name,
           email: auth_provider&.[](:credentials)&.[](:email),
           password: auth_provider&.[](:credentials)&.[](:password)
       )
+      {
+          user: u,
+          result: u.errors.blank?
+      }
     end
   end
 end
