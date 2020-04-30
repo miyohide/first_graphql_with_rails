53cfb81aa1708de3608db55e7e3e465380073b7c
Hidenori Miyoshi <miyohide@gmail.com>
2020-04-30 16:47:55 +0900

    add signin_user mutation.

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,5 +2,6 @@ module Types
   class MutationType < Types::BaseObject
     field :postPhoto, mutation: Mutations::PostPhoto
     field :create_user, mutation: Mutations::CreateUser
+    field :signin_user, mutation: Mutations::SignInUser
   end
 end
